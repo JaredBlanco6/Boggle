b7aa1dc543a66fe839b0560a556ad9af53d0a8bd
JaredBlanco6 <jaredblanco6@icloud.com>
2019-11-24 11:11:27 -0500

    fixed the bug in my tree where some words flags werent being flipped

--- tree_dictionary.c
+++ tree_dictionary.c
@@ -66,24 +66,61 @@ int main(int argc, char* argv[]){
 
   //scans in our dicitonary and adds all of the letters from words into our tree
   make_tree(argv[1], &root);
+  print_list(root); printf("\n");
+  print_list(root->children); printf("\n");
+  print_list(root->children->children); printf("\n");
 
+  tree_t *temp = root->children->children->children;
+  //print_list(root->children->children->children); printf("\n");
+
+  /*
+  while(temp != NULL){
+    printf("%c %d   ", temp->letter, temp->is_word);
+    temp = temp->next;
+  }
+  */
 
   //recursivly goes through the list, every time we find a is_word flag to be true, we print char's starting from parent
-  print_found_words(root);
-  printf("\n");
+  //print_found_words(root);
+  //printf("\n");
 
-  if(word_check(&root, "eatability")){
+
+
+  if(word_check(&root, "HELLO")){
     printf("WOOOOO\n");
   }else{
-    printf("word not found\n");
+    printf("hello word not found\n");
   }
 
-  if(word_check(&root, "aut")){
+  if(word_check(&root, "AALII")){
     printf("WOOOOO\n");
   }else{
-    printf("word not found\n");
+    printf("stuffword not found\n");
+  }
+  /*
+  tree_t *temp = (root)->children;
+
+  while(temp->letter != 'H'){
+    temp = temp->next;
+  }
+  temp = temp->children;
+  while(temp->letter != 'E'){
+    temp = temp->next;
+  }
+  temp = temp->children;
+  while(temp->letter != 'L'){
+    temp = temp->next;
+  }
+  temp = temp->children;
+  while(temp->letter != 'L'){
+    temp = temp->next;
+  }
+  while(temp){
+    printf("%c with flag %d\n", temp->letter, temp->is_word);
+    temp = temp->next;
   }
 
+  */
 
   freeList(&root);
   return 0;
@@ -93,23 +130,25 @@ int word_check(tree_t **root, char word[]){
   short index = 0;
   short max_index = strlen(word) - 1;
 
+
   //if our word is not atleast 3 letters log we dont care about it
   if(max_index < 2){
     return 0;
   }
 
+
   tree_t *temp = (*root)->children;
 
   while(index != max_index){
-
     //scroll to the word in the list
     while((temp != NULL) && (temp->letter != word[index])){
       temp = temp->next;
     }
 
     //we did not find the letter after scrolling and looking
+    //printf("this one %c\n", temp->letter);
     if((temp == NULL) || (temp->letter != word[index])){
-      return 0;
+      return 0; printf("we did not find  it\n");
     }
 
     //we found the letter
@@ -122,8 +161,9 @@ int word_check(tree_t **root, char word[]){
 
   //we are have made it down the list until the last letter, now we check if it's a word
   if(temp->letter == word[index]){
-    //printf("%c %c\n",word[index], temp->letter);
-    return temp->is_word;
+    return temp->is_word; printf("right here\n");
+  }else{
+    printf("we did not find the letter\n");
   }
 
 
@@ -167,7 +207,7 @@ void decalre_root(tree_t **root){
   short word_size = 2;
 
   //makes a base tree of the alphabet
-  for(int i = 'a'; i <= 'z'; i++){
+  for(int i = 'A'; i <= 'Z'; i++){
     word[1] = i;
     add_children(&(*root), word, word_size, 0);
   }
@@ -236,19 +276,30 @@ void addNode(tree_t*parent, char new_letter, int is_word){
 
   //scroll to the end of the list
   while(current_node->next != NULL){
+
     //if the letter is already in the list, we don't add the node;
     if(current_node->letter == new_letter){
+      //IF we are not going to add this duplicate but the flag is true for antoher word, we take the true flag
+      if(current_node->is_word == 0){
+        current_node->is_word = is_word; //printf("Flipping flag\n");
+      }
       free(new_child);
       return;
     }
+
     current_node = current_node->next;
   }
 
+
   //ensuring no duplicates
   if(current_node->letter == new_letter){
+    if(current_node->is_word < is_word){
+      current_node->is_word = is_word; //printf("flipping flag %c\n", current_node->letter);
+    }
     free(new_child);
     return;
   }
+
 
   //adds our new node to our list
   current_node->next = new_child;
